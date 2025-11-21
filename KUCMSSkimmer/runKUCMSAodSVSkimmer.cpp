7b2ca5b63cfadeab873f8489d0ff25c748a23d1e
Jack King <jking79@ku.edu>
2025-11-21 12:46:25 -0600

    Preparing for first condor submission

--- KUCMSSkimmer/runKUCMSAodSVSkimmer.cpp
+++ KUCMSSkimmer/runKUCMSAodSVSkimmer.cpp
@@ -32,9 +32,9 @@ int main ( int argc, char *argv[] ){
 	int evtj = 0;
 	bool useEvtGenWgt = true;
 	bool noSVorPho = false; // ( must have a sig pho or a sv ) + base selection - loads photons in rjr
-    	bool genSigPerfect = false;
-    	bool hasGenInfo = false;
-    	string outfilename = "rjrskim_v39"; // included BHC processing and branchs, added triggerpath varibles
+    bool genSigPerfect = false;
+    bool hasGenInfo = false;
+    string outfilename = "rjrskim"; // included BHC processing and branchs, added triggerpath varibles
 	string key = "";
 	float xsec = 1;
 	float glumass = 0;
@@ -49,14 +49,14 @@ int main ( int argc, char *argv[] ){
         	if(strncmp(argv[i],"-h", 2) == 0)     			{ hprint = true; }
         	if(strncmp(argv[i],"-i", 2) == 0)     			{ i++; in_file = string(argv[i]); }
         	if(strncmp(argv[i],"--input", 7) == 0)			{ i++; in_file = string(argv[i]); }
-		if(strncmp(argv[i],"--output", 8) == 0)			{ i++; outfilename = string(argv[i]); }
+			if(strncmp(argv[i],"--output", 8) == 0)			{ i++; outfilename = string(argv[i]); }
         	if(strncmp(argv[i],"-o", 2) == 0)				{ i++; outfilename = string(argv[i]); }
-		if(strncmp(argv[i],"--evtFirst", 6) == 0)		{ i++; evti = std::atoi(argv[i]); }
+			if(strncmp(argv[i],"--evtFirst", 6) == 0)		{ i++; evti = std::atoi(argv[i]); }
         	if(strncmp(argv[i],"--evtLast", 6) == 0)		{ i++; evtj = std::atoi(argv[i]); }
-		if(strncmp(argv[i],"--hasGenInfo", 12) == 0)	{ hasGenInfo = true; }
-		if(strncmp(argv[i],"--genSigPerfect", 15) == 0)	{ genSigPerfect = true; }
-		if(strncmp(argv[i],"--noSVorPho", 11) == 0)		{ noSVorPho = true; }
-		if(strncmp(argv[i],"--noBHC", 7) == 0)		{ noBHC = true; }
+			if(strncmp(argv[i],"--hasGenInfo", 12) == 0)	{ hasGenInfo = true; }
+			if(strncmp(argv[i],"--genSigPerfect", 15) == 0)	{ genSigPerfect = true; }
+			if(strncmp(argv[i],"--noSVorPho", 11) == 0)		{ noSVorPho = true; }
+			if(strncmp(argv[i],"--noBHC", 7) == 0)		{ noBHC = true; }
         	if(strncmp(argv[i],"--dataSetKey", 12) == 0)	{ i++; key = string(argv[i]); }
         	if(strncmp(argv[i],"--xsec", 6) == 0)			{ i++; xsec = std::stof(argv[i]); }
         	if(strncmp(argv[i],"--gluinoMass", 12) == 0)	{ i++; glumass = std::stof(argv[i]); }
@@ -71,19 +71,19 @@ int main ( int argc, char *argv[] ){
         	cout << "  options:" << endl;
         	cout << "   --help(-h)                           print options" << endl;
         	cout << "   --input(-i) [file]                   input file" << endl;
-		cout << "   --output(-o) [file]                  output file tag" << endl;
-		cout << "   --evtFirst [i] --evtLast [j]         skim from event i to event j (default evtFirst = evtLast = 0 to skim over everything)" << endl;
-		cout << "   --hasGenInfo                         sample has gen info (default = false)" << endl;
-		cout << "   --genSigPerfect                      turn on gensigperfect (default = false)" << endl;
-		cout << "   --noSVorPho                          selection doesn't have SV or photon (default = false)" << endl;
-		cout << "   --noBHC                              will not run BHC objects (default = false - BHC on)" << endl;
+			cout << "   --output(-o) [file]                  output file tag" << endl;
+			cout << "   --evtFirst [i] --evtLast [j]         skim from event i to event j (default evtFirst = evtLast = 0 to skim over everything)" << endl;
+			cout << "   --hasGenInfo                         sample has gen info (default = false)" << endl;
+			cout << "   --genSigPerfect                      turn on gensigperfect (default = false)" << endl;
+			cout << "   --noSVorPho                          selection doesn't have SV or photon (default = false)" << endl;
+			cout << "   --noBHC                              will not run BHC objects (default = false - BHC on)" << endl;
         	cout << "   --dataSetKey                         set dataset key" << endl;
         	cout << "   --xsec                               set cross-section (default = 1)" << endl;
         	cout << "   --gluinoMass                         set gluino mass (default = 0)" << endl;
         	cout << "   --N2Mass                             set N2 mass (default = 0)" << endl;
         	cout << "   --timeCaliTag                        set time calibration tag (default = r2_ul18(_mc))" << endl;
         	cout << "   --MCweight                           set MC weight (default = 0)" << endl;
-		cout << "   --MCtype [type]			 set MC type (default = 0)" << endl;
+			cout << "   --MCtype [type]			 set MC type (default = 0)" << endl;
 		return 0;
 	}
 
@@ -95,26 +95,26 @@ int main ( int argc, char *argv[] ){
     	int skipCnt = 0; // used to skip files ( in tchian ) for fast processing - if( nFiles%skipCnt != 0 ) continue; --  disabled in code  --  
     	std::string eosdir = "root://cmseos.fnal.gov//store/user/lpcsusylep/jaking/";
     	KUCMSAodSkimmer llpgana;
-	llpgana.SetEventRange(evti, evtj);
-	llpgana.SetGenInfoFlag( hasGenInfo );
+		llpgana.SetEventRange(evti, evtj);
+		llpgana.SetGenInfoFlag( hasGenInfo );
     	llpgana.SetNoSVorPhoFlag( noSVorPho );
     	llpgana.SetUseEvtGenWgtFlag( useEvtGenWgt );
     	llpgana.SetGenSigPerfectFlag( genSigPerfect );
     	llpgana.SetDoBHC( !noBHC );
-	//from master list
-   	llpgana.SetDataSetKey(key);
+		//from master list
+   		llpgana.SetDataSetKey(key);
     	llpgana.SetCrossSection(xsec);
     	llpgana.SetGluinoMass(glumass);
     	llpgana.SetN2Mass(n2mass);
     	llpgana.SetMCType(mctype);
     	llpgana.SetTimeCalibrationTag(ttag);
-	llpgana.SetMCWeight(mcw);
-	//this method takes in 1 list at a time
-	eosdir = eosdir+"KUCMSNtuple/";
-	if(in_file.find(eosdir) == string::npos)
-	       in_file = eosdir+in_file;	
+		llpgana.SetMCWeight(mcw);
+		//this method takes in 1 list at a time
+		eosdir = eosdir+"KUCMSNtuple/";
+		if(in_file.find(eosdir) == string::npos)
+	    in_file = eosdir+in_file;	
         llpgana.kucmsAodSkimmer( in_file, outfilename);
-    return 1;
 
+    return 1;
 
 }//<<>>int main ( int argc, char *argv[] )
