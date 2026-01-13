690310ff67de299559f1a0bbbb91ca0d5694b4f2
Justin Anguiano <justin.anguiano1@gmail.com>
2026-01-13 13:21:19 -0600

    added job resubmission tools with CheckJobs after v45 skim runs

--- KUCMSSkimmer/condor/runhadds.sh
+++ KUCMSSkimmer/condor/runhadds.sh
@@ -2,12 +2,12 @@
 #PROC=DiPJBox
 #PROC=GJets
 #PROC=QCD
-PROC=SMS
+#PROC=SMS
 #PROC=TTXJets
 #PROC=WJets
-#PROC=ZJets
+PROC=ZJets
 
-#python3 doHadd_Multi.py --dir ./testOutput --proc ${PROC}
-#source haddScripts/doHadd_${PROC}_.sh
+python3 doHadd_Multi.py --dir ./testOutput --proc ${PROC}
+source haddScripts/doHadd_${PROC}_.sh
 
 python3 doXRDCP.py --dir ./testOutput --proc ${PROC} --targetdir skims_v45 #--dryRun
